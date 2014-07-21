141b64c53e71fcf6d12fedbe00f29d3dca664f0f
Saleem Abdulrasool <abdulras@fb.com>
2014-07-21 12:10:53 -0700

    Add missing include
    Add a stdint.h inclusion for the (type)definition of intptr_t.

--- fishhook.h
+++ fishhook.h
@@ -22,6 +22,7 @@
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include <stddef.h>
+#include <stdint.h>
 
 /*
  * A structure representing a particular intended rebinding from a symbol
